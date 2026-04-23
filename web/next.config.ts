216773e392b7ff3002da8fd381f4264495b9b9e1
haja <haja@xtuae.com>
2026-04-23 12:02:32 +0530

    Configured subpath basePath for static export and added APP URL

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,6 +4,7 @@ const isStatic = process.env.IS_STATIC_EXPORT === 'true';
 
 const nextConfig: NextConfig = {
   output: isStatic ? 'export' : undefined,
+  basePath: isStatic ? '/eurovanta' : undefined,
   images: {
     unoptimized: isStatic,
   },
